78d0b927fdbc80efe157dd355858855f664e268e
SuyogMahajan <mahajansuyog9322@gmail.com>
2022-02-07 11:57:40 +0530

    added task activity

--- app/src/main/java/com/example/mytodo/MainActivity.kt
+++ app/src/main/java/com/example/mytodo/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.mytodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mytodo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.floatingActionButton.setOnClickListener {
+
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/mytodo/TaskActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mytodo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TaskActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task)
+    }
+}
